f1ec9cffe6f8ab5509699e19461e56bdcfa5af59
Сергей Темников <temnikov.s@list.ru>
2024-06-04 20:22:14 +0500

    first commit

---
+++ lab13var17/AddPersonWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace lab13var17
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPersonWindow.xaml
+    /// </summary>
+    public partial class AddPersonWindow : Window
+    {
+
+        PeoplesEntities db;
+        byte[] image;
+
+        public AddPersonWindow(PeoplesEntities db)
+        {
+            InitializeComponent();
+            this.db=db;
+        }
+
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog(); 
+            if (openFile.ShowDialog() == true) 
+            {
+                image = File.ReadAllBytes(openFile.FileName); 
+                imgPhoto.Source = new ImageSourceConverter().ConvertFrom(openFile.FileName) as ImageSource;
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            People people = new People();
+
+            people.Surname = txbSurname.Text;
+            people.Age = int.Parse(txbAge.Text);
+            people.Soldier = txbSoldier.Text;
+            people.Image = image;
+
+            try
+            {
+                db.People.Add(people);
+                db.SaveChanges();
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось записать человека");
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ lab13var17/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab13var17
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PeoplesEntities db = new PeoplesEntities();
+
+        List<People> peoplesDgList = new List<People>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dgPeoples.ItemsSource = db.People.ToList();
+
+        }
+
+        private void btnYoungPerson_Click(object sender, RoutedEventArgs e)
+        {
+            peoplesDgList.Clear();
+            peoplesDgList.Add(db.People.Where(x => x.Soldier == "Да").OrderBy(x => x.Age).FirstOrDefault());
+            dgPeoples.ItemsSource = null;
+            dgPeoples.ItemsSource = peoplesDgList;
+        }
+
+        private void btnOldPeople_Click(object sender, RoutedEventArgs e)
+        {
+            peoplesDgList.Clear();
+            peoplesDgList.Add(db.People.Where(x => x.Soldier == "Нет").OrderByDescending(x => x.Age).FirstOrDefault());
+            peoplesDgList.Add(db.People.Where(x => x.Soldier == "Да").OrderByDescending(x => x.Age).FirstOrDefault());
+            dgPeoples.ItemsSource = null;
+            dgPeoples.ItemsSource = peoplesDgList;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var surname = txbSurnameSearch.Text;
+            peoplesDgList.Clear();
+            foreach(var p in db.People.Where(x => x.Surname == surname))
+            {
+                peoplesDgList.Add(p);
+            }
+            dgPeoples.ItemsSource = null;
+            dgPeoples.ItemsSource = peoplesDgList;
+        }
+
+        private void btnCancelSearch_Click(object sender, RoutedEventArgs e)
+        {
+            dgPeoples.ItemsSource = null;
+            dgPeoples.ItemsSource = db.People.ToList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddPersonWindow window = new AddPersonWindow(db);
+            window.ShowDialog();
+            dgPeoples.ItemsSource = null;
+            dgPeoples.ItemsSource = db.People.ToList();
+        }
+    }
+}
